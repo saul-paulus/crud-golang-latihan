614092c095355106de1c426a8ac1e512d104117b
saul paulus <saulpaulus17@gmail.com>
2020-12-11 01:17:08 +0900

    membuat get dan create data member

--- main.go
+++ main.go
@@ -6,16 +6,48 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Member struct {
+	Id         string `json:"id"`
+	CodeMember string `json:"codeMember"`
+	NickName   string `json:"nickName"`
+	LastName   string `json:"lastName"`
+	Status     bool   `json:"status"`
+}
+
+var member []Member
+
 func main() {
 	router := gin.Default()
 	router.GET("/", homePage)
-	router.GET("/api/v1/member", getReadMember)
+	router.GET("/api/v1/datamember", getMember)
+	router.POST("/api/v1/member", getCreateMember)
 
 	router.Run(":5000")
 }
 
 func homePage(c *gin.Context) {
-	c.AsciiJSON(http.StatusOK, map[string]interface{}{
+	c.JSON(http.StatusOK, map[string]interface{}{
 		"Message": "Success",
+	})
+}
+
+func getMember(c *gin.Context) {
+	c.JSON(200, member)
+
+}
+
+func getCreateMember(c *gin.Context) {
+	var reqBody Member
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(422, gin.H{
+			"error":   true,
+			"message": "Invalid request body",
+		})
+		return
+	}
+	member = append(member, reqBody)
+
+	c.JSON(200, gin.H{
+		"message": "Success",
 	})
 }
