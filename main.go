4bd35a588bd635dfa5ddbd5ab797fb855bc4d942
saul paulus <saulpaulus17@gmail.com>
2020-12-13 14:12:26 +0900

    menambahkan edit dan delete

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type Member struct {
@@ -19,8 +20,12 @@ var member []Member
 func main() {
 	router := gin.Default()
 	router.GET("/", homePage)
-	router.GET("/api/v1/datamember", getMember)
+
+	router.GET("/api/v1/member", getMember)
+	router.GET("/api/v1/member/:id", getIdMember)
 	router.POST("/api/v1/member", getCreateMember)
+	router.PUT("/api/v1/member/:id", getEditMember)
+	router.DELETE("/api/v1/member/:id", getDeleteMember)
 
 	router.Run(":5000")
 }
@@ -45,9 +50,61 @@ func getCreateMember(c *gin.Context) {
 		})
 		return
 	}
+
+	reqBody.Id = uuid.New().String() //Menggenerate ID secara otomatis
+
 	member = append(member, reqBody)
 
 	c.JSON(200, gin.H{
 		"message": "Success",
+	})
+}
+
+func getEditMember(c *gin.Context) {
+	id := c.Param("id")
+	var reqBody Member
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(422, gin.H{
+			"error":   true,
+			"message": "Invalid request body",
+		})
+		return
+	}
+	for index, idMember := range member {
+		if idMember.Id == id {
+			member[index].CodeMember = reqBody.CodeMember
+			member[index].NickName = reqBody.NickName
+			member[index].LastName = reqBody.LastName
+			member[index].Status = reqBody.Status
+
+			c.JSON(200, map[string]interface{}{
+				"message": "Success",
+			})
+			return
+		}
+		c.JSON(404, gin.H{
+			"error":   true,
+			"message": "invalid to edit",
+		})
+	}
+}
+
+func getDeleteMember(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, u := range member {
+		if u.Id == id {
+			member = append(member[:i], member[i+1:]...)
+
+			c.JSON(200, map[string]interface{}{
+				"message": "success",
+			})
+			return
+
+		}
+	}
+	c.JSON(404, gin.H{
+		"error":   true,
+		"Message": "invalid delete member data",
 	})
 }
